24836e52d95d6285f08236a84ee17145a0b1874e
rainverted <MY_NAME@example.com>
2021-05-19 20:09:03 +0300

    render socials #14

--- js/home.js
+++ js/home.js
@@ -1,5 +1,9 @@
 //IMPORT
 import { Clock } from "./components/clock/Clock.js";
+import { socials } from "./components/socials/socials.js";
+import { socialsData } from "./data/socialsData.js";
+
 
 //EXECUTION
 Clock('.clock');
+socials('footer .socials', socialsData);
