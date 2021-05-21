b7b76ee63b0ba88c2ab1ceae0189f2e2bddea288
rainverted <MY_NAME@example.com>
2021-05-21 16:40:34 +0300

    Laiko formatavimas

--- js/components/clock/Clock.js
+++ js/components/clock/Clock.js
@@ -1,6 +1,73 @@
-function Clock (selector) {
+class Clock {
+    constructor(selector, targetDate) {
+        this.selector = selector;
+        this.targetDate = targetDate;
+
+        this.DOM = null;
+
+        this.init();
+    }
+
+    init() {
+        if (!this.isValidSelector()) {
+            return false;
+        }
+
+        this.render();
+    }
+
+    isValidSelector() {
+        if (typeof this.selector !== 'string' ||
+            this.selector === '') {
+            console.error('ERROR: selector has to be non-empty string');
+            return false;
+        }
+
+        this.DOM = document.querySelector(this.selector);
+        if (!this.DOM) {
+            console.error('ERROR: could not find an element by given selector');
+            return false;
+        }
+
+        return true;
+    }
+
+    formatTime(timeValues) {
+        const updatedTime = [];
+
+        for (let i = 0; i < timeValues.length; i++) {
+            const time = timeValues[i];
+            if (i === 0 || time > 9) {
+                // kada pushinam originalu skaiciu
+                updatedTime.push(time);
+            } else {
+                // o kada pridedam nuli priekyje
+                updatedTime.push('0' + time);
+            }
+        }
+
+        return updatedTime;
+    }
+
+    render() {
+        const timeValues = this.formatTime([2, 9, 37, 39]);
+        const labelValues = ['Days', 'Hours', 'Minutes', 'Seconds'];
+        let HTML = '';
+
+        for (let i = 0; i < timeValues.length; i++) {
+            HTML += `<div class="time">
+                        <div class="value">${timeValues[i]}</div>
+                        <div class="label">${labelValues[i]}</div>
+                    </div>`;
+        }
+
+        this.DOM.innerHTML = HTML;
+    }
+}
+
+/*function Clock (selector) {
     const DOM = document .querySelector(selector);
-    /*const HTML = `<div class="time">
+    const HTML = `<div class="time">
                 <div class="value">432</div>
                 <div class="label">days</div>
                 </div>
@@ -18,7 +85,7 @@ function Clock (selector) {
                 </div>
                 <div class="time">
                     
-                </div>`*/
+                </div>`
 
         const timeValues = [432, 9, 37, 39];
         const labelVaues = ['Days', 'Hours', 'Minutes', 'Seconds'];
@@ -36,6 +103,6 @@ function Clock (selector) {
 
     DOM.innerHTML = HTML; 
     //innerHTML paima kinatmajo HTML viduje esanti teksta 
-}
+}*/
 
 export { Clock }
--- js/home.js
+++ js/home.js
@@ -1,9 +1,10 @@
 //IMPORT
 import { Clock } from "./components/clock/Clock.js";
 import { socials } from "./components/socials/socials.js";
+import { clockData } from "./data/clockData.js";
 import { socialsData } from "./data/socialsData.js";
 
 
 //EXECUTION
-Clock('.clock');
+new Clock('.clock', clockData);
 socials('footer .socials', socialsData);
